0392f84398215b0f3da412fcb40a554031ae844b
Adacat <Kyo0604hei@gmail.com>
2025-07-09 14:30:24 +0900

    updateMail

--- scripts/get_refresh_token.js
+++ scripts/get_refresh_token.js
@@ -5,7 +5,7 @@ const readline = require("readline");
 // 1) ここに先ほど取得したクライアントID／シークレットを入れる
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI = "http://localhost:3000/api/auth/callback"; // OAuth設定と合わせる
+const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 
 const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
